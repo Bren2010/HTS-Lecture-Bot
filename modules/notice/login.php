05d25cae3648752d910883c4d8df885f7019b081
Bren2010 <admin@bren2010.com>
2010-10-22 16:42:56 -0400

    Fixed problem with identifying.

--- modules/notice/login.php
+++ modules/notice/login.php
@@ -9,7 +9,7 @@ return function($message) {
 		
 		if ($registered == TRUE && $message->getNick() == "NickServ" && $text == "If you do not change within one minute, I will change your nick.") {
 			global $NSpassword;
-			say("privmsg", "NickServ", "IDENTIFY " . $NSpassword);
+			cmd_send("PRIVMSG NickServ :IDENTIFY " . $NSpassword);
 		}
 	}
 	
